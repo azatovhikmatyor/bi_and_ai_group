735078ba863f00f1cdeeab2aa900cf7328c06d01
Hikmatyor Azatov <azatovhikmatyor@gmail.com>
2024-10-23 22:13:36 +0500

    first lesson files

---
+++ first.py
@@ -0,0 +1,36 @@
+# print(6 + 4 * 2)
+# print(9 / 3) # / - bo'lish amali
+# print(10 // 3) # // - butun bo'lish amali
+# print(4**3)     # ** - darajaga ko'tarish amali
+# print(10 / 0) # ZeroDivisionError
+
+a = 3
+print(type(a))  # int
+
+b = 3.8
+print(type(b))  # float
+
+c = 4 + 8j
+print(type(c))  # complex
+
+d = "This is string"
+print(type(d)) # str
+
+e = True # False
+print(type(e)) # bool
+
+
+# 5 - integer number
+# 5.7 - flating point number
+# 4 + 5j - complex number
+# "Hello" - string
+# True, False - boolean
+
+
+a = 5
+b = 5
+print("a = 5")
+print("id(a) =", id(a))
+print("id(b) =", id(b))
+
+# Console = Terminal = Command Line
---
+++ type casting.py
@@ -0,0 +1,39 @@
+# print('Program Started')
+# name = input("Enter name: ")
+
+# name = "5"
+
+# print("The name is :", name)
+# print(type(name))
+
+# x = "5"
+# print(type(x))
+# print(x)
+
+# y = int(x)
+# print(type(y))
+# print(y)
+
+# z = int(4.8)
+# print(z)
+
+# print(type(int("Hello")))
+# int, float, bool, str
+print(int("7"))
+print(int(7.6))
+print(int(True))
+print(int(False))
+print("------------")
+print(float("5.8"))
+print(float(3))
+print("-----------")
+print(bool(1))
+print(bool(0))
+print(bool(2))
+print(bool(""))
+print(bool("asdfa"))
+print("-----------")
+print(type(str(89)))
+print(type(str(5.4)))
+print(type(str("sdgsd")))
+print(type(str(True)))
