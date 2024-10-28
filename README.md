4a75cb90f72bb6e4379767cae7ff23aee2dee3bf
Hikmatyor Azatov <azatovhikmatyor@gmail.com>
2024-10-28 22:21:49 +0500

    lesson3

---
+++ README.md
@@ -0,0 +1,7 @@
+# This is title
+
+## This is subtitle
+
+### This is another paragraph title
+
+<h1 style="color:blue">Hello World<h1>
---
+++ lesson 3/demo.py
@@ -0,0 +1,17 @@
+# Primitive data types:
+# Number
+int
+float
+complex
+
+str
+bool: False, True
+NoneType: None
+
+# Container data types
+list
+tuple
+set
+dict
+
+
